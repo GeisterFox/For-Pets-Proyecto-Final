bfdefc4c4b159d99007f4b30f2cdda622fc7564d
GeisterFox <julineira123@gmail.com>
2024-11-09 05:47:09 -0300

    Add files via upload

---
+++ js/productos.js
@@ -0,0 +1,75 @@
+// productos.js
+const productos = [
+    {
+        nombre: "Antiparasitario - Perros/Gatos",
+        descripcion: "Antihelmíntico nematodicida para cachorros y gatitos.",
+        precio: 7800,
+        imagen: "./img/Tienda/AntiP.webp"
+    },
+    {
+        nombre: "Royal Canin - Puppy",
+        descripcion: "Para perros cachorros, hasta los 10 meses de edad.",
+        precio: 26775,
+        imagen: "./img/Tienda/Mini.webp"
+    },
+	{
+        nombre: "Cortauñas - Perros/Gatos",
+        descripcion: "Alicate con sistema de seguridad, color azul/negro.",
+        precio: 5500,
+        imagen: "./img/Tienda/CortaU.webp"
+    },
+	{
+        nombre: "VitalCan - Gatos adultos y castrados",
+        descripcion: "Contribuye a reducir la grasa corporal y cuidar las articulaciones con una fórmula baja en calorías.",
+        precio: 31500,
+        imagen: "./img/Tienda/GatoC.webp"
+    },
+	{
+        nombre: "Purina - Bajas Calorias",
+        descripcion: "Facilita la pérdida de peso en forma sana y protege a largo plazo la salud general.",
+        precio: 95795,
+        imagen: "./img/Tienda/PerroC.webp"
+    },
+	{
+        nombre: "Pipeta - Perros",
+        descripcion: "Protege contra pulgas y garrapatas.",
+        precio: 3800,
+        imagen: "./img/Tienda/Pipeta.webp"
+    },
+	{
+        nombre: "Platito",
+        descripcion: "Platito resistente a la corrosión y duradero.",
+        precio: 12000,
+        imagen: "./img/Tienda/Plato.webp"
+    },
+    {
+        nombre: "Rascador",
+        descripcion: "Rascador casero con apariencia de tubo.",
+        precio: 73000,
+        imagen: "./img/Tienda/Rasca.webp"
+    },
+	{
+        nombre: "Bolsos transportadores",
+        descripcion: "Bolsos de lona con ventanita, para cualquier mascota.",
+        precio: 35000,
+        imagen: "./img/Tienda/Bolso.webp"
+    },
+	{
+        nombre: "Huesos de cuero",
+        descripcion: "Huesos para masticar y de todas las edades.",
+        precio: 3600,
+        imagen: "./img/Tienda/Hueso.webp"
+    },
+	{
+        nombre: "Comida para peces",
+        descripcion: "Shulet de 10gr, altamente palatable y no enturbia el agua.",
+        precio: 4000,
+        imagen: "./img/Tienda/Peces.webp"
+    },
+	{
+        nombre: "Eukanuba - Perros adultos",
+        descripcion: "Para perros adultos mayores a 12 meses de edad.",
+        precio: 60180,
+        imagen: "./img/Tienda/PerroA.webp"
+    }
+];
